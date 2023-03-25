4ce0c875b77c4b5aa4f25734c837bb43d8ff7345
EitMD <113485843+EitMD@users.noreply.github.com>
2023-03-24 21:12:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -19,11 +19,17 @@ logging.info(api_mlparams)
 def infer():  
     reqX_1 = request.args.get('x_1')
     reqX_2 = request.args.get('x_2')
+    reqX_3 = request.args.get('x_3')
+    reqX_4 = request.args.get('x_4')
     x_1 = float(reqX_1)
     x_2 = float(reqX_2)
+    x_3 = float(reqX_3)
+    x_4 = float(reqX_4)
     print((x_1,type(x_1)),file=sys.stderr)
     print((x_2,type(x_2)),file=sys.stderr)
-    X_in = np.ndarray(shape=(1,2),buffer=np.array([x_1, x_2]) )
+    print((x_3,type(x_3)),file=sys.stderr)
+    print((x_4,type(x_4)),file=sys.stderr)
+    X_in = np.ndarray(shape=(1,4),buffer=np.array([x_1, x_2, x_3, x_4]) )
     return {'y':api_mlparams.predict(X_in).item()}
 
 if __name__ == '__main__':
