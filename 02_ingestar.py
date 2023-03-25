426c3f87194dfd74045d560b13ebaece4c93499e
EitMD <113485843+EitMD@users.noreply.github.com>
2023-03-24 20:46:21 -0600

    Update 02_ingestar.py

--- 02_ingestar.py
+++ 02_ingestar.py
@@ -2,32 +2,33 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+# Salvar el dataset
 def save_data(set_name,dataset):
-	dataset.to_csv(set_name + ".csv",index=False)
-	print("Dataset "+ set_name +".csv listo ...")
+  dataset.to_csv(set_name + ".csv", index=False, columns=['lat','long','depth','stations','mag'])
+  print("Dataset "+ set_name +".csv listo ...")
 
-def fix_data(A,B):
-  ds= pd.DataFrame()
-  ds= A
-  ds['test_result']=B
-  return ds
+# Junta los datos en un dataset
+def fix_data(A,B,dataset):
+  ds= pd.DataFrame(data = A)
+  ds[target_val]=B
+  print(ds.head(5))
+  save_data(dataset,ds)
 
+# Dividir datos en 3 datasets
 def split_data(dataframe_name, validation=False, ss=0.1):
-  datasets= pd.DataFrame()
   df = pd.read_csv(dataframe_name)
-  X = df.drop('test_result',axis=1) 
-  y = df['test_result']
-  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=ss, random_state=101)
+  X = df.drop(target_val,axis=1) 
+  y = df[target_val]
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=ss, random_state=42)
   if validation == False: 
     setname_01='train'
     setname_02='test'
   else :
-    setname_01='train_01'
-    setname_02='train_02'
-  datasets=fix_data(X_train,y_train)
-  save_data(setname_01,datasets)
-  datasets=fix_data(X_test,y_test)
-  save_data(setname_02,datasets)
+    setname_01='train'
+    setname_02='validate'
+  fix_data(X_train,y_train,setname_01)
+  fix_data(X_test,y_test,setname_02)
 
-split_data('quakes.csv', validation = False, ss = 0.2)
-split_data('train.csv', validation = True, ss = 0.1)
+# llamada de funciones
+split_data(dataset_name, validation = False, ss = 0.2)
+split_data(dataset_name_01, validation = True, ss = 0.1)
