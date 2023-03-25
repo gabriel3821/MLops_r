a755d588a45daf024ae4901e80127bc9d62d4b02
EitMD <113485843+EitMD@users.noreply.github.com>
2023-03-24 21:51:49 -0600

    Update 03_entrenar.py

--- 03_entrenar.py
+++ 03_entrenar.py
@@ -2,10 +2,55 @@ import pandas as pd
 import pickle
 from sklearn.linear_model import LinearRegression
 
+import rpy2
+import rpy2.robjects as ro
+import rpy2.robjects.packages as rpacks
+from rpy2.robjects.packages import importr
+
 # Definicion de valores
 target_val = "mag"
 ds_train = "quakes.csv"
 ds_validate = "validate.csv"
+
+# Validacion de correlacion R
+def corr_validate_2(val_name):
+
+  # Importar la librería stats de R
+  stats = rpacks.importr('stats')
+
+  # Importar los datos en R
+  ro.r('datos_r <- read.csv("validate.csv")')
+
+  # Calcular las correlaciones
+  cor_mag_lat = stats.cor_test(ro.r('datos_r$mag'), ro.r('datos_r$lat'), method='pearson')
+  cor_mag_long = stats.cor_test(ro.r('datos_r$mag'), ro.r('datos_r$long'), method='pearson')
+  cor_mag_depth = stats.cor_test(ro.r('datos_r$mag'), ro.r('datos_r$depth'), method='pearson')
+  cor_mag_stations = stats.cor_test(ro.r('datos_r$mag'), ro.r('datos_r$stations'), method='pearson')
+
+  # Obtener los resultados de las correlaciones
+  print('mag_lat')
+  coef_corr_mag_lat = cor_mag_lat.rx2('estimate')[0]
+  p_valor_mag_lat = cor_mag_lat.rx2('p.value')[0]
+  print('Coeficiente de correlación:', coef_corr_mag_lat)
+  print('P-valor:', p_valor_mag_lat)
+
+  print('mag_long')
+  coef_corr_mag_long = cor_mag_long.rx2('estimate')[0]
+  p_valor_mag_long = cor_mag_long.rx2('p.value')[0]
+  print('Coeficiente de correlación:', coef_corr_mag_long)
+  print('P-valor:', p_valor_mag_long)
+
+  print('mag_depth')
+  coef_corr_mag_depth = cor_mag_depth.rx2('estimate')[0]
+  p_valor_mag_depth = cor_mag_depth.rx2('p.value')[0]
+  print('Coeficiente de correlación:', coef_corr_mag_depth)
+  print('P-valor:', p_valor_mag_depth)
+
+  print('mag_stations')
+  coef_corr_mag_stations = cor_mag_stations.rx2('estimate')[0]
+  p_valor_mag_stations = cor_mag_stations.rx2('p.value')[0]
+  print('Coeficiente de correlación:', coef_corr_mag_stations)
+  print('P-valor:', p_valor_mag_stations)
 
 # Desplegar metircas de pesos, intercepcion, r cuadrada
 def data_metrics_2(X_01, y_01, modelo):
@@ -64,3 +109,6 @@ def validation(val_name, filename='mlparams'):
 
 training(ds_train)   
 validation(ds_validate)
+corr_validate_2(ds_validate)
+
+
